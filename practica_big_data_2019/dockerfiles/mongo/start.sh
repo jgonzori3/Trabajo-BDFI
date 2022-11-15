229bfb793726e43ddd6647bc5a03f1a17a1efe77
jgonzori3 <jgonzori3@github.com>
2022-11-15 17:26:40 +0000

    primer commit con dockerfiles y docker compose

---
+++ practica_big_data_2019/dockerfiles/mongo/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+systemctl start mongod
+  
+
+
--- practica_big_data_2019/flight_prediction/src/main/scala/es/upm/dit/ging/predictor/MakePrediction.scala
+++ practica_big_data_2019/flight_prediction/src/main/scala/es/upm/dit/ging/predictor/MakePrediction.scala
@@ -21,7 +21,7 @@ object MakePrediction {
     import spark.implicits._
 
     //Load the arrival delay bucketizer
-    val base_path= "/home/user1/trabajoBDFI/practica_big_data_2019"
+    val base_path= "/home/user1/Desktop/PruebasDocker/Trabajo-BDFI/practica_big_data_2019"
     val arrivalBucketizerPath = "%s/models/arrival_bucketizer_2.0.bin".format(base_path)
     print(arrivalBucketizerPath.toString())
     val arrivalBucketizer = Bucketizer.load(arrivalBucketizerPath)
